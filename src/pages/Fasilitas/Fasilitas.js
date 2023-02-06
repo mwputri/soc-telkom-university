f168dee91102a901eda8c794dcf70af77bbf8299
myfruitmango <123440759+myfruitmango@users.noreply.github.com>
2023-02-06 14:15:59 +0700

    Add files fasilitas
    >_<

---
+++ src/pages/Fasilitas/Fasilitas.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+import "./Fasilitas.css";
+import fasilitas1 from "../../assets/img/fasilitas/fasilitas1.webp";
+import fasilitas2 from "../../assets/img/fasilitas/fasilitas2.webp";
+import fasilitas3 from "../../assets/img/fasilitas/fasilitas3.webp";
+import fasilitas4 from "../../assets/img/fasilitas/fasilitas4.webp";
+import fasilitas5 from "../../assets/img/fasilitas/fasilitas5.webp";
+
+import fasilitas6 from "../../assets/img/fasilitas/fasilitas6.webp";
+import fasilitas7 from "../../assets/img/fasilitas/fasilitas7.webp";
+import fasilitas8 from "../../assets/img/fasilitas/fasilitas8.webp";
+import fasilitas9 from "../../assets/img/fasilitas/fasilitas9.webp";
+import fasilitas10 from "../../assets/img/fasilitas/fasilitas10.webp";
+import fasilitas11 from "../../assets/img/fasilitas/fasilitas11.webp";
+import fasilitas12 from "../../assets/img/fasilitas/fasilitas12.webp";
+import fasilitas13 from "../../assets/img/fasilitas/fasilitas13.webp";
+
+import fasilitas14 from "../../assets/img/fasilitas/fasilitas14.webp";
+import fasilitas15 from "../../assets/img/fasilitas/fasilitas15.webp";
+import fasilitas16 from "../../assets/img/fasilitas/fasilitas16.webp";
+import fasilitas17 from "../../assets/img/fasilitas/fasilitas17.webp";
+import fasilitas18 from "../../assets/img/fasilitas/fasilitas18.webp";
+import fasilitas19 from "../../assets/img/fasilitas/fasilitas19.webp";
+
+import fasilitas20 from "../../assets/img/fasilitas/fasilitas20.webp";
+import fasilitas21 from "../../assets/img/fasilitas/fasilitas21.webp";
+import fasilitas22 from "../../assets/img/fasilitas/fasilitas22.webp";
+import fasilitas23 from "../../assets/img/fasilitas/fasilitas23.webp";
+import fasilitas24 from "../../assets/img/fasilitas/fasilitas24.webp";
+import fasilitas25 from "../../assets/img/fasilitas/fasilitas25.webp";
+import fasilitas26 from "../../assets/img/fasilitas/fasilitas26.webp";
+import fasilitas27 from "../../assets/img/fasilitas/fasilitas27.webp";
+
+const Fasilitas = () => {
+  return (
+    <div>
+      <div className="p-lg-5" style={{ overflowX: "auto" }}>
+        <div className="content-fasilitas">
+          <h2>Gedung Perkulihan</h2>
+          <div className="m-auto flex flex-col lg:flex-row items-center justify-evenly w-full">
+            <img className="lg:w-40 mt-8" src={fasilitas1} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas2} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas3} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas4} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas5} alt="" />
+          </div>
+          <h2>Laboratorium Riset</h2>
+          <div className="m-auto flex flex-col lg:flex-row items-center justify-evenly w-full">
+            <img className="lg:w-40 mt-8" src={fasilitas6} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas7} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas8} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas9} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas10} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas11} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas12} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas13} alt="" />
+          </div>
+          <h2>Fasilitas Olahraga</h2>
+          <div className="m-auto flex flex-col lg:flex-row items-center justify-evenly w-full">
+            <img className="lg:w-40 mt-8" src={fasilitas14} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas15} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas16} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas17} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas18} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas19} alt="" />
+          </div>
+          <h2>Lingkungan Fakultas Informatika</h2>
+          <div className="m-auto flex flex-col lg:flex-row items-center justify-evenly w-full">
+            <img className="lg:w-40 mt-8" src={fasilitas20} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas21} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas22} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas23} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas24} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas25} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas26} alt="" />
+            <img className="lg:w-40 mt-8" src={fasilitas27} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Fasilitas;
