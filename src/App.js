c8b5eaade80480b749c7322311edbfd6bb3ea45f
myfruitmango <123440759+myfruitmango@users.noreply.github.com>
2023-02-06 14:17:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Civitas from './pages/Civitas/Civitas'
 import LokasiKami from './pages/Lokasi-Kami/Lokasi-Kami'
 import VisiMisi from './pages/VisiMisi/VisiMisi'
 import Rencana from './pages/Rencana/Rencana'
+import Sejarah from './pages/Sejarah/Sejarah'
+import Fasilitas from './pages/Fasilitas/Fasilitas'
 import data from 'assets/data/data.json'
 import Footer from "./components/Footer/";
 
@@ -32,6 +34,8 @@ function App() {
                 <Route path="/lokasi-Kami" element={<LokasiKami data={lectureData} handleChange={handleSearch}/>}/>
                 <Route path="/visi-misi" element={<VisiMisi data={lectureData} handleChange={handleSearch}/>}/>
                 <Route path="/rencana-strategis-fakultas-informatika" element={<Rencana data={lectureData} handleChange={handleSearch}/>}/>
+                <Route path="/sejarah-fakultas-informatika" element={<Sejarah data={lectureData} handleChange={handleSearch}/>}/>
+                <Route path="/fasilitas" element={<Fasilitas data={lectureData} handleChange={handleSearch}/>}/>
             </Routes>
             <Footer/>
 
