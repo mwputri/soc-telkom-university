4489f77f589d8fd787c91f53be974d339d5494ee
myfruitmango <123440759+myfruitmango@users.noreply.github.com>
2023-02-08 18:05:51 +0700

    update routh path dan data

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import VisiMisi from './pages/VisiMisi/VisiMisi'
 import Rencana from './pages/Rencana/Rencana'
 import Sejarah from './pages/Sejarah/Sejarah'
 import Fasilitas from './pages/Fasilitas/Fasilitas'
-import data from 'assets/data/data.json'
 import Footer from "./components/Footer/";
 import LabRiset from './pages/Lab-Riset/Lab-Riset'
 import PrestasiMahasiswa from './pages/Prestasi-Mahasiswa/Prestasi-Mahasiswa'
 import PrestasiDosen from './pages/Prestasi-Dosen/Prestasi-Dosen'
 import StaffAdministrasi from 'pages/Staff-Administrasi/Staff-Administrasi';
+
+import data from 'assets/data/data.json'
 import DataPD from 'assets/data/pd.json'
 import DataPM from 'assets/data/pm.json'
 import DataSA from 'assets/data/sa.json'
@@ -22,6 +23,9 @@ import DataSA from 'assets/data/sa.json'
 
 function App() {
     const [lectureData, setLectureData] = useState();
+    const [lecturerachievement,  setlecturerachievement] = useState();
+    const [studentachievement,  Setstudentachievement] = useState();
+    const [administrativestaff,  Setadministrativestaff] = useState();
     const handleSearch = e => {
         const res = data
             .filter((x) => x.NAMA_LENGKAP.toLowerCase().includes(e.target.value.toLowerCase()) || x.KODE_DOSEN?.toLowerCase().includes(e.target.value.toLowerCase()))
@@ -31,6 +35,9 @@ function App() {
 
     useEffect(() => {
         setLectureData(data)
+        setlecturerachievement(prestasidosen)  
+        Setstudentachievement(prestasimahasiswa)
+        Setadministrativestaff(staffadmin)
     }, [])
 
     return (
@@ -39,15 +46,16 @@ function App() {
             <Routes>
                 <Route path="/" element={<AboutUs/>}/>
                 <Route path="/Dosen" element={<Dosen data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/lokasi-Kami" element={<LokasiKami data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/visi-misi" element={<VisiMisi data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/rencana-strategis-fakultas-informatika" element={<Rencana data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/sejarah-fakultas-informatika" element={<Sejarah data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/fasilitas" element={<Fasilitas data={lectureData} handleChange={handleSearch}/>}/>
+                <Route path="/lokasi-Kami" element={<LokasiKami />}/>
+                <Route path="/visi-misi" element={<VisiMisi />}/>
+                <Route path="/rencana-strategis-fakultas-informatika" element={<Rencana/>}/>
+                <Route path="/sejarah-fakultas-informatika" element={<Sejarah/>}/>
+                <Route path="/fasilitas" element={<Fasilitas/>}/>
+                <Route path="/struktur-organisasi" element={<StrukturOrganisasi/>}/>
                 <Route path="/Lab-Riset" element={<LabRiset data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/Prestasi-Mahasiswa" element={<PrestasiMahasiswa data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/Prestasi-Dosen" element={<PrestasiDosen data={lectureData} handleChange={handleSearch}/>}/>
-                <Route path="/Staff-Administrasi" element={<StaffAdministrasi data={lectureData} handleChange={handleSearch}/>}/>
+                <Route path="/Prestasi-Mahasiswa" element={<PrestasiMahasiswa data={studentachievement} handleChange={handleSearch}/>}/>
+                <Route path="/Prestasi-Dosen" element={<PrestasiDosen data={lecturerachievement} handleChange={handleSearch}/>}/>
+                <Route path="/Staff-Administrasi" element={<StaffAdministrasi data={administrativestaff} handleChange={handleSearch}/>}/>
             </Routes>
             <Footer/>
 
