4f1f816f7d0055dc26a37c6fd065c7e3cb60adbb
MW Putri Meilinia <mwputrimeiliniahp@gmail.com>
2023-02-08 11:22:50 +0700

    yok

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -73,7 +73,7 @@ const Navbar = () => {
                                                                 ))}
                                                             </ul>
                                                         </div>
-                                                    </>
+                                                    </div>
                                                     :
                                                     <a href={subDetail.path}
                                                        className="block px-4 py-2 hover:bg-gray-50 hover:text-black text-left">{subDetail.name}
